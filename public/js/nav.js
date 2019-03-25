a52e9304224127e04bcbe75b80267dc60f4936ac
booldook <booldook@gmail.com>
2019-03-25 15:42:32 +0900

    ppt

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,23 +1,20 @@
-$("#navs1 > li").click(function(){
-	$(this).find(".subs").slideToggle(100);
+$("#navs1 > li").mouseover(function(){
+	$(this).find(".subs").slideDown(1000);
 });
-
-$("#navs1 > li").hover(function(){
-	$(this).find(".subs").slideDown(100);
-}, function(){
-	$(this).find(".subs").slideUp(100);
+$("#navs1 > li").mouseleave(function(){
+	$(this).find(".subs").slideUp(1000);
 });
 
 $("#navs2 > ul > li").mouseover(function(){
-	$(".subs2").slideDown(100);
+	$(".subs2").slideDown(1000);
 });
 $("#navs2").mouseleave(function(){
-	$(".subs2").slideUp(100);
+	$(".subs2").slideUp(1000);
 });
 
 $("#navs3 .mn_nav").mouseover(function(){
-	$(".subs3").slideDown(100);
+	$(".subs3").slideDown(1000);
 });
 $("#navs3 > ul").mouseleave(function(){
-	$(".subs3").slideUp(100);
+	$(".subs3").slideUp(1000);
 });
