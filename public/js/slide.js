44c5067cdbe55879090dfada8c513e865c9d78d9
booldook <booldook@gmail.com>
2019-03-26 14:37:16 +0900

    pager

--- public/js/slide.js
+++ public/js/slide.js
@@ -16,8 +16,19 @@ $("#slides1").mouseleave(function(){
 	interval = setInterval(ani1, 3000);
 });
 
+var k2 = -100;
 var wid2 = 0;
 var interval2 = setInterval(ani2, 3000);
 function ani2() {
-	
+	wid2 += k2;	//wid2 = wid2 + k2 -> wid2 = 0 - 100
+	$("#slides2").stop().animate({"left": wid2+"%"}, 500, function(){
+		if(wid2 == 0) k2 = -100;
+		if(wid2 == -400) k2 = 100;
+	});
 }
+$("#slides2").mouseover(function(){
+	clearInterval(interval2);
+});
+$("#slides2").mouseleave(function(){
+	interval2 = setInterval(ani2, 3000);
+});
