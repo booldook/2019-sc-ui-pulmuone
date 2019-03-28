def49be57e81134b09e589a51bb330e0ab011be2
booldook <booldook@gmail.com>
2019-03-28 12:16:11 +0900

    navs 완성

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
 /***** Main 네비게이션 *****/
 $(".mn_navs > li").mouseover(function(){
 	$(".sb_navs_wraps").show();
+	$(".sb_hover").find("i").hide();
+	$(".sb_hover").eq($(this).index()).find("i").show();
 });
 $(".navs_wraps").mouseleave(function(){
 	$(".sb_navs_wraps").hide();
+});
+
+$(".sb_hover").mouseover(function(){
+	$(this).css({"background-color":"#f9fbf6"});
+	$(this).find("i").show();
+});
+$(".sb_hover").mouseleave(function(){
+	$(this).css({"background-color":"#fff"});
+	$(".sb_hover").find("i").hide();
 });
