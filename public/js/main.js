82051466e82e1b0d8a5940b45c2c0d138fdf6eb7
booldook <booldook@gmail.com>
2019-03-28 15:16:55 +0900

    sbMedal

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 /***** Main 네비게이션 *****/
+var sbMedal = ['gnb_sec01_img0_on.png', 'gnb_sec01_img1_on.png', 'gnb_sec01_img2_on.png', 'gnb_sec01_img3_on.png', 'gnb_sec01_img4_on.png', 'gnb_sec01_img5_on.png'];
 $(".mn_navs > li").mouseover(function(){
 	$(".sb_navs_wraps").show();
 	$(".sb_hover").find("i").hide();
 	$(".sb_hover").eq($(this).index()).find("i").show();
+	$(".sb_medal").attr("src", "../img/"+sbMedal[$(this).index()]);
 });
 $(".navs_wraps").mouseleave(function(){
 	$(".sb_navs_wraps").hide();
@@ -12,6 +14,7 @@ $(".sb_hover").mouseover(function(){
 	$(this).css({"background-color":"#f9fbf6"});
 	$(".sb_hover").find("i").hide();
 	$(this).find("i").show();
+	$(".sb_medal").attr("src", "../img/"+sbMedal[$(this).index() - 1]);
 });
 $(".sb_hover").mouseleave(function(){
 	$(this).css({"background-color":"#fff"});
